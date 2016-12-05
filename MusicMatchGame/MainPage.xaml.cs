e31502632a4ed4de9941bf52837ffe4bdba3adb4
shaneyg8 <shanegleeson22@gmail.com>
2016-12-05 19:58:52 +0000

    re-alignment

--- MusicMatchGame/MainPage.xaml.cs
+++ MusicMatchGame/MainPage.xaml.cs
@@ -25,9 +25,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace MusicMatchGame
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    
     public sealed partial class MainPage : Page
     {
         private ObservableCollection<Song> Songs;
